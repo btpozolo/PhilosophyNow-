e963160c42fbeeefd1af9b5877cff8c9bb93a335
Blake Pozolo <blake.pozolo@gmail.com>
2022-08-02 22:40:26 -0700

    Initial Commit

---
+++ PhilosophyNow!/PhilosophyNow_App.swift
@@ -0,0 +1,17 @@
+//
+//  PhilosophyNow_App.swift
+//  PhilosophyNow!
+//
+//  Created by Blake Pozolo on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PhilosophyNow_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
